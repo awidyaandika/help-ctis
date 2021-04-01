27e2f4f24c6688cacd4ffffc445ba506fc4bb9f8
awidyaandika <awidyaandika@gmail.com>
2021-04-01 10:03:53 +0800

    HELP CTIS v1.1.5
    - Add link to view test centre on home manger

--- helpctis/resources/views/manager/handleManager.blade.php
+++ helpctis/resources/views/manager/handleManager.blade.php
@@ -35,7 +35,7 @@
                         <div class="icon">
                             <i class="ion ion-home"></i>
                         </div>
-                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        <a href="{{route('view-testcentre')}}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
                 <!-- ./col -->
